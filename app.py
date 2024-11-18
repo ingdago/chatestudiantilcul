14a6681f2a5b008eac71487940ebd2f9a11301e9
ingdago <54037963+ingdago@users.noreply.github.com>
2024-11-18 14:13:18 -0500

    Update 7 app.py

--- app.py
+++ app.py
@@ -527,4 +527,7 @@ def get_messages():
     return {'messages': message_list}
 
 if __name__ == '__main__':
+    import eventlet
+    eventlet.monkey_patch()
     socketio.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
